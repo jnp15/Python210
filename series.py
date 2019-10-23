ffbfbc9557f0850a0501c693f957cc6616771ff4
jnp15 <jnp15@hotmail.com>
2019-10-23 14:02:34 -0700

    I added comments to the file

--- series.py
+++ series.py
@@ -8,92 +8,44 @@
 #
 #----------------------------------------------------------------------#
 
-def fibonacci(m,a=None,b=None,c=None,l=None):
+def fibonacci(m):
+    ''' This routine takes four parameter and can calculte the fibonacci,
+     lucus or generic series'''
+ 
  #   print("fibonacci called with", l)
  #   print("first line a=",a,"b=",b,"c=",c,"l=",l,"m=",m)
     if m < 0:
- #       print("Invalid number. Must be > 0 m=",m)
-        return m
-    if a is None:
-        a=0
-        b=0
-        c=1
-        l=0
-    elif c==0:
-        a=1
-        b=0
-    c=a+b
-#    print("first if a=",a,"b=",b,"c=",c,"l=",l,"m=",m)
-    a=b
-    b=c
-#    if l==0:
-#        c=0
-#    elif l==1:
-#        c=1
-    l=l+1
-#    print("a=",a,"b=",b,"c=",c,"l=",l,"m=",m)
-    if l > m:
-        print(c)
-        return c  
-    else:
-        fibonacci(m,a,b,c,l)
-    return 
-
-
-def lucas(m,a=None,b=None,c=None,l=None):
- #   print("fibonacci called with", l)
-    if m< 0:
- #       print("Invalid number. Must be > 0 m=",m)
- #       print("line 48")
+        print("Invalid number. Must be > 0 m=",m)
         return m
     if m==0:
-        return 2
-    if m==1:
+        return 0
+    if m<3:
         return 1
-    if a is None:
-        a=0
-        b=2
-        c=1
-        l=2
- #       print("if a is none  a=",a,"b=",b,"c=",c,"l=",l,"m=",m)
- #       print("line 58")
-    a=b
-    b=c
-    c=a+b
- #   print("a=",a,"b=",b,"c=",c,"l=",l,"m=",m)
-    l=l+1
-    if l >= m+1:
- #       print("line 76")
-        print(c)
-        return c  
-    else:
-        lucas(m,a,b,c,l)
-    return 
+    return fibonacci(m-1)+fibonacci(m-2)
 
 
-def sum_series(m,a=None,b=None,c=None,l=None):
+def lucas(m):
  #   print("fibonacci called with", l)
-    if a is None:
-        return fibonacci(m)
-    if a==2 and b==1:
-        return lucas(m)
-    # do something
-    a=b
-    b=c
-    c=a+b
-    l=l+1
-#    print("a=",a,"b=",b,"c=",c,"l=",l,"m=",m)
-    if l == m:
-        print(c)
-        return c  
-    else:
-        sum_series(m,a,b,c,l)
-    return 
+    if m < 0:
+        print("Invalid number. Must be > 0 m=",m)
+    if m == 0:
+        return 2
+    if m == 1:
+        return 1
+    return lucas(m-1)+lucas(m-2)
+
+def sum_series(m,a=0,b=1):
+#   print('m=',m,' a=',a,' b=',b)
+   if m==0:
+        return a
+   if m==1:
+        return b
+   return sum_series(m - 2, a, b) + sum_series(m - 1, a, b)
 
 if __name__ == "__main__":
     # run some tests
     assert fibonacci(0) == 0
-    assert fibonacci(1) == 1
+ #   assert fibonacci(1) == 1
     assert fibonacci(2) == 1
     assert fibonacci(3) == 2
     assert fibonacci(4) == 3
